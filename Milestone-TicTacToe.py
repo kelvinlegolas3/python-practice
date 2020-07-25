e2d3c5ea275e550152edde38918d46b2e26193ae
Kevin Nicholas Almario <kevinnicholas.almario@infor.com>
2020-07-26 01:39:56 +0800

    Added current player

--- Milestone-TicTacToe.py
+++ Milestone-TicTacToe.py
@@ -12,7 +12,7 @@ def change_table(table, symbol):
     is_input_valid = False
 
     while not is_input_valid:
-        first_move, second_move = input("What's your move?: ").split()
+        first_move, second_move = input(f"What's your move? [{symbol}]: ").split()
         try:
             if table[int(first_move)][int(second_move)] == " ":
                 is_input_valid = True
@@ -57,7 +57,6 @@ def is_game_over(table):
     possibilities = list(filter(validate_combination, combinations))
     return len(possibilities) > 0
 
-    print(fuck)
 
 # Tic Tac Toe - Game
 player1_symbol = input("Choose 1st player's symbol: ")
