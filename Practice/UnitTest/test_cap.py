061b490c0fc29fb3f548516d227dabf1f1e83ff7
Kevin Nicholas Almario <kevinnicholas.almario@infor.com>
2020-08-05 19:13:02 +0800

    Create unit test for capitalized words

--- Practice/UnitTest/test_cap.py
+++ Practice/UnitTest/test_cap.py
@@ -1,7 +1,17 @@
-from unittest import TestCase
+import unittest
 import cap
 
-class TestCap(TestCase):
+class TestCap(unittest.TestCase):
+    
     def test_one_word(self):
         text = 'python'
-        result = cap.cap
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Python')
+    
+    def test_multiple_words(self):
+        text = 'kevin almario'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Kevin Almario')
+        
+if __name__ == '__main__':
+    unittest.main()
